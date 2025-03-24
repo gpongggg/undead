b432a51fdb83368f398809745056c7007f8700c3
LeBeaBae <lebeabae@gmail.com>
2025-03-23 22:43:56 -0500

    OMFG

---
+++ js/apps.js
@@ -0,0 +1,82 @@
+// Here all versions are located, the same as app.js but this time its apps.js because all versions info is located in this one file
+// To add more versions, copy the versions.v1 and replace with versions.v2, and the same with others (it support more than 9 versions)
+
+versions.v1 = {
+  name: "Hello",
+  version: "OMFG-Hello",
+  date: "2025",
+  folder: "asset-v1/",
+  looptime: 4561,
+  bpm: 105,
+  totalframe: 220,
+  nbpolo: 7,
+  bonusloopA: true,
+  bonusendloopA: true,
+  colBck: "#250721",
+  col0: "#c44ab2",
+  col1: "#a0298f",
+  col2: "#842176",
+  col3: "#5d1954",
+  col4: "#250721",
+  animearray: [
+    {
+      name: "1_kick",
+      color: "66CEF6",
+      uniqsnd: !0,
+    },
+    {
+      name: "2_snare",
+      color: "D0E077",
+      uniqsnd: !0,
+    },
+    {
+      name: "3_hatty",
+      color: "7360AA",
+      uniqsnd: !0,
+    },
+    {
+      name: "4_bassy",
+      color: "F3754D",
+      uniqsnd: !0,
+    },
+    {
+      name: "5_piano",
+      color: "5D3112",
+      uniqsnd: !0,
+    },
+    {
+      name: "6_bell",
+      color: "A9C5E7",
+      uniqsnd: !1,
+    },
+    {
+      name: "7_wewewo",
+      color: "E99DC5",
+      uniqsnd: !0,
+    },
+    {
+      name: "8_owewewo",
+      color: "FDE8D5",
+      uniqsnd: !1,
+    },
+    {
+      name: "9_howareyou",
+      color: "F37778",
+      uniqsnd: !1,
+    },
+    {
+      name: "10_amazing",
+      color: "34C4EC",
+      uniqsnd: !1,
+    },
+  ],
+  bonusarray: [
+    {
+      name: "Hello",
+      src: "b1-v1-hello-hb",
+      code: "1,2,3,4,9",
+      sound: "bonus-hello",
+      loop: 2,
+    },
+  ],
+};
